a29b71be1ccdf44c979993ff1521a964128bc8f4
Jonathan <gdk2012@gmail.com>
2022-10-15 20:19:34 -0700

    add gui

--- js/script.js
+++ js/script.js
@@ -1,7 +1,52 @@
 let computerSelection, playerSelection;
 let playerScore = 0; 
 let computerScore = 0;
-game();
+let whosePoint = '';
+const scoreBoard = document.querySelector('#scoreBoard');
+const playerBoard = document.querySelector('.playerBoard');
+const computerBoard = document.querySelector('.computerBoard');
+const results = document.querySelector('#results');
+const newP = document.createElement('p');
+const newPoint = document.createElement('p');
+const newBtn = document.createElement('button');
+newBtn.textContent = "New Game";
+let newGame = true;
+
+if (newGame) {
+    let btnRock = document.getElementById('rock');
+    let btnPaper = document.getElementById('paper');
+    let btnScissors = document.getElementById('scissors');
+    btnRock.addEventListener('click', function() { playRound('rock'); });
+    btnPaper.addEventListener('click', function() { playRound('paper'); });
+    btnScissors.addEventListener('click', function() { playRound('scissors'); });
+    playerScore, computerScore = 0;
+    newGame = false;
+    // results.newBtn.remove()
+};
+
+
+function endGame() {
+
+        btnRock.removeEventListener('click', function() { playRound()});
+        btnPaper.removeEventListener('click', playRound());
+        btnScissors.removeEventListener('click', playRound());
+        results.append(newBtn);
+}
+
+// game();
+
+// const btns = document.querySelectorAll('.move');
+// btns.forEach(move => move.addEventListener('click'))
+
+
+
+
+// document.getElementById('rock').addEventListener('click', playRound(console.log("rock")));
+
+
+
+
+
 
 function getComputerChoice() {
     let num = Math.floor(Math.random() * 100);
@@ -21,33 +66,60 @@ function getPlayerChoice () {
     }
     }
 
-function playRound(computerSelection, playerSelection) {
+function playRound(playerSelection) {
+    // getPlayerChoice();
+    getComputerChoice();
+    // console.log(computerSelection);
     if (computerSelection === playerSelection) {
-        console.log("Tie");
+        // console.log("Tie");
         } else if (computerSelection === "rock") {
             if (playerSelection === "paper") {
             playerScore++;
-            console.log("Player point");
+            whosePoint = "Player point";
         } else {
             computerScore++;
-            console.log("Computer point");
+            whosePoint = "Computer point";
         }
         } else if (computerSelection === "paper") {
             if (playerSelection === "rock") {
                 computerScore++;
-                console.log("Computer point");
+                whosePoint = "Computer point";
             } else {
                 playerScore++;
-                console.log("Player point");
+                whosePoint = "Player point";
             }
         } else if (computerSelection === "scissors") {
             if (playerSelection === "rock") {
                 playerScore++;
-                console.log("Player point");
+                whosePoint = "Player point";
             } else {
                 computerScore++;
-                console.log("Computer point");
+                whosePoint = "Computer point";
             }
+        }
+        if (whosePoint === '') {
+            whosePoint = 'Tie';
+        }
+        playerBoard.textContent = "Player score: " + playerScore;
+        computerBoard.textContent = "Computer score: " + computerScore;
+        results.appendChild(newP);
+        results.appendChild(newPoint);
+        newP.textContent = "You threw " + playerSelection + " & computer threw " + computerSelection;
+        newPoint.textContent = whosePoint;
+        whosePoint = '';
+        if (playerScore > 4 || computerScore > 4) {
+            if (playerScore > computerScore) {
+                winner = "Player";
+            } else {
+                winner = "Computer";
+            }
+            newGame = true;
+            newP.textContent = "Game over!! " + winner + " wins!!" 
+            endGame();
+
+
+            
+            return;
         }
     }
 
@@ -65,3 +137,4 @@ function game() {
         console.log("Computer wins!")
     }
 } 
+
